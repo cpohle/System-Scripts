2c5ceb7a71ef60e151faec3a954777f621428223
Christian Pohle <pohle@pohle.com>
2020-05-22 17:26:09 +0200

    Create umlauts.js

---
+++ JavaScript/umlauts.js
@@ -0,0 +1,14 @@
+Ä %C4
+Ö %D6
+Ü %DC
+
+ä %E4
+ö %F6
+ü %FC
+
+ß %DF
+
+{
+	unescape('Sehen Sie sich die wichtigsten Funktionen in einer kleinen Tour durch die App an!')
+}
+text: unescape('%DCberspringen')
