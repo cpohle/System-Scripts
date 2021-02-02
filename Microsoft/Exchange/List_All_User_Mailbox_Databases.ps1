6c9e55d74b2e46d44da9ea6e563fe7749419b7c5
Christian Pohle <pohle@pohle.com>
2021-02-02 11:28:51 +0100

    Create List_All_User_Mailbox_Databases.ps1

---
+++ Microsoft/Exchange/List_All_User_Mailbox_Databases.ps1
@@ -0,0 +1,8 @@
+﻿# List all User Mailbox Databases
+
+# Load Snap In (when called in PowerShell ISE and not in Exchange Management Shell)
+# Snapin for Exchange 2013 and later
+  Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn; 
+
+# Show current Database of Users
+  Get-mailbox -ResultSize Unlimited | select SamAccountName, alias, database | export-csv ".\User_Mailbox_Databases.csv" -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";"
