c07db4a130a53448840470bf380300a25a0117aa
Christian Pohle <pohle@pohle.com>
2021-02-02 13:05:45 +0100

    Create CitrixServersBatchPatch.ps1

---
+++ Citrix/Provisioning Services (PVS)/CitrixServersBatchPatch.ps1
@@ -0,0 +1,18 @@
+﻿# List all Citrix Farm Devices with Collection Name and Type, ready for txt Import to Batchpatch 
+
+# Load PowerShell SnapIn 
+Import-Module “C:\Program Files\Citrix\Provisioning Services Console\Citrix.PVS.SnapIn.dll”
+
+# List all Devices ready for Import to BatchPatch
+  $citrixservers = Get-PvsDeviceInfo | select DeviceName,CollectionName,Type
+
+  ForEach ($server in $citrixservers)
+     {
+#      Store in Variables
+       $DeviceName = $server.DeviceName
+       $CollectionName = $server.CollectionName
+       $Type = $server.Type
+
+#      Write for BatchPatch
+       Write “$DeviceName |$CollectionName ||$Type” >> ./CitrixServersBatchPatch.txt
+     } 
