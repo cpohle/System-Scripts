e5199096453bd96c6380544fbd56aa668b4539b3
Christian Pohle <pohle@pohle.com>
2021-02-02 12:24:19 +0100

    Create CitrixServers.ps1

---
+++ Citrix/Provisioning Services (PVS)/CitrixServers.ps1
@@ -0,0 +1,7 @@
+﻿# List all Citrix Farm Devices with Collection Name and Type, ready for Export to Excel 
+
+# Load PowerShell SnapIn 
+Import-Module “C:\Program Files\Citrix\Provisioning Services Console\Citrix.PVS.SnapIn.dll”
+
+# List all Devices ready for Export to Excel 
+Get-PvsDeviceInfo | select DeviceName,CollectionName,Type  | export-csv ".\CitrixServers.csv" -Append -NoTypeInformation -Encoding UTF8 -Delimiter ";" 
